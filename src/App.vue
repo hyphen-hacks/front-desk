fa538eb78cf8cfe6fc292877780bec5e72498470
Ronan <rsf.sho@gmail.com>
2019-10-11 19:29:17 -0700

    loading bugs

--- src/App.vue
+++ src/App.vue
@@ -5,14 +5,15 @@
       <h1 class="brand">Hyphen-Hacks Front Desk v<span>{{version}}</span></h1>
       <a class="toDash" href="https://dashboard.hyphen-hacks.com">dashboard</a>
     </nav>
-    <loading v-if="!roster.length"></loading>
+    <loading v-if="loading"></loading>
     <router-view/>
   </div>
 </template>
 <script>
   import 'minireset.css'
   import './assets/app.scss'
-import loading from './components/loading'
+  import loading from './components/loading'
+
   export default {
     name: 'appContainer',
     components: {loading},
@@ -20,15 +21,32 @@ import loading from './components/loading'
       return {
         version: require('../package.json').version,
         roster: [],
-        rosterObject: {}
+        rosterObject: {},
+        user: false,
+        userLoaded: false
+      }
+    },
+    computed: {
+      loading() {
+        if (this.userLoaded) {
+          if (this.user) {
+            return this.roster.length === 0
+          } else {
+            return false
+          }
+        } else {
+          return true
+        }
+
       }
     },
     created() {
       this.$firebase.auth().onAuthStateChanged(user => {
+
         if (user) {
+          this.user = user
           this.$firebase.firestore().collection('people').onSnapshot(snap => {
             console.log('got data')
-            this.loading = false;
             this.roster = []
             snap.forEach(person => {
               this.roster.push(person.data())
@@ -37,9 +55,11 @@ import loading from './components/loading'
 
           })
         } else {
+          this.user = false
           console.log('no user')
           this.$router.push('/login')
         }
+        this.userLoaded = true
       })
     }
 
